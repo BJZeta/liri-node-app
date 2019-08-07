d7e67ad631b69912ec2697ecf4732e42b62b1e0e
Brandon U <unzueta.brandon@gmai.com>
2019-08-06 19:04:22 -0700

    Added OMDB function

--- liri.js
+++ liri.js
@@ -24,27 +24,42 @@ if (input === "spotify-this") {
     spotify.search({ type: 'track', query: search_term })
         .then(function (response) {
             var track = response.tracks.items[0]
-            console.log('\n--------------------------------\n');
-            console.log("Artist Name: " + track.artists[0].name);
+            console.log('\n--------------------------------');
+            console.log("Artist Name : " + track.artists[0].name);
             console.log('--------------------------------');
             console.log("Song Name : " + track.name);
             console.log('--------------------------------');
             console.log("Preview Link : " + track.external_urls.spotify);
             console.log('--------------------------------');
-            console.log("Album: " + track.album.name);
-            console.log('\n--------------------------------\n');
+            console.log("Album : " + track.album.name);
+            console.log('--------------------------------\n');
         })
 
 } else if (input === "movie-this") {
-    var movieURL = 'https://www.omdbapi.com/?t=' + search_term + '&apikey=trilogy';
     axios.get('https://www.omdbapi.com/?t=' + search_term + '&apikey=trilogy').
-    then(function (response) {
-        data = response.data;
-        console.log(data)
-        
-    }).catch(function (err) {
-        console.log(err)
-    })
+        then(function (response) {
+            data = response.data;
+            console.log('\n--------------------------------');
+            console.log("Title : " + data.Title);
+            console.log('--------------------------------');
+            console.log("Release Date : " + data.Released);
+            console.log('--------------------------------');
+            console.log("IMDB Rating : " + data.imdbRating);
+            console.log('--------------------------------');
+            console.log("Rotten Tomato Rating : " + data.Ratings[1].Value);
+            console.log('--------------------------------');
+            console.log("Produced In : " + data.Country);
+            console.log('--------------------------------');
+            console.log("Language : " + data.Language);
+            console.log('--------------------------------');
+            console.log("Plot : " + data.Plot);
+            console.log('--------------------------------');
+            console.log("Cast : " + data.Actors);
+            console.log('--------------------------------\n');
+
+        }).catch(function (err) {
+            console.log(err)
+        })
 } else if (input === "do-what-it-says") {
 
 }
