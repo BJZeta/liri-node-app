f32221a9dfc645140b06f74731f567e6a0ec7f09
Brandon U <unzueta.brandon@gmai.com>
2019-10-16 17:59:43 -0700

    adding final changes to app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+## This is my liri app that uses the the BandsInTown, Omdb, and Spotify api
+
+This app allows a user to search song info on a song name, venue information on a band/artist, and imdb info on a movie title 
+
+## To search for a song info....
+  ### Type in 'spotify-this-song (song name)'
+
+## To search for info on a concert for a band/artist....
+  ### Type in 'concert-this (band/artist name)'
+  
+## To search for movie info....
+  ### Type in 'movie-this (movie title)'
--- liri.js
+++ liri.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 var keys = require('./keys');
 var axios = require('axios');
-var Spotify = require('node-spotify-api')
+var Spotify = require('node-spotify-api');
 var fs = require('fs');
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -13,14 +14,22 @@ var search_term = process.argv.slice(3).join(" ");
 
 
 
-// if (input === "concert-this") {
-//     var concertURL = "https://rest.bandsintown.com/artists/" + search_term + "/events?app_id=codingbootcamp"
-//     axios.get(concertURL).
-//         then(function (response) {
-//             var band_in_town = response.data;
-//             console.log(band_in_town);
-//         })
-if (input === "spotify-this") {
+if (input === "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + search_term + "/events?app_id=codingbootcamp").
+        then(function (response) {
+            var venueDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log('\n--------------------------------');
+            console.log("Venue Information:");
+            console.log('\n--------------------------------');
+            console.log("Name : " + response.data[0].venue.name);
+            console.log('\n--------------------------------');
+            console.log("Location : " + response.data[0].venue.city + ", " + response.data[0].venue.country);
+            console.log('\n--------------------------------');
+            console.log("Date : " + venueDate);
+            console.log('\n--------------------------------');
+        })
+    }
+if (input === "spotify-this-song") {
     spotify.search({ type: 'track', query: search_term })
         .then(function (response) {
             var track = response.tracks.items[0]
@@ -46,7 +55,7 @@ if (input === "spotify-this") {
             console.log('--------------------------------');
             console.log("IMDB Rating : " + data.imdbRating);
             console.log('--------------------------------');
-            console.log("Rotten Tomato Rating : " + data.Ratings[1].Value);
+            console.log("Rotten Tomato Rating : " + data.Ratings[0].Value);
             console.log('--------------------------------');
             console.log("Produced In : " + data.Country);
             console.log('--------------------------------');
@@ -61,5 +70,24 @@ if (input === "spotify-this") {
             console.log(err)
         })
 } else if (input === "do-what-it-says") {
-
+    fs.readFile("random.txt",'utf-8', function(err,data) {
+        if(err) throw err;
+        console.log(data);
+        var text = data.split(',');
+        spotify.search({ type: 'track', query: text[1] })
+        .then(function (response) {
+            var track = response.tracks.items[0]
+            console.log('\n--------------------------------');
+            console.log("Artist Name : " + track.artists[0].name);
+            console.log('--------------------------------');
+            console.log("Song Name : " + track.name);
+            console.log('--------------------------------');
+            console.log("Preview Link : " + track.external_urls.spotify);
+            console.log('--------------------------------');
+            console.log("Album : " + track.album.name);
+            console.log('--------------------------------\n');
+        })
+    })
+} else {
+    console.log('\n---------------------------\n Welcome to LIRI :)')
 }
